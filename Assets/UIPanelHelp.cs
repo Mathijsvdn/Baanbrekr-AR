355207f140f7806c72d1df30ed280c9a58ed41f7
Mathijsvdn <mvdnieuwenhoff@gmail.com>
2025-05-25 15:35:17 +0200

    Hij heeft hem weer in mn onedrive opgeslagen
    kut windows, elke dag denk ik aan linux...

---
+++ Assets/UIPanelHelp.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPanelHelp : MonoBehaviour
+{
+    public GameObject originPanel;
+    public GameObject helpPanel;
+
+    void Start()
+    {
+        if (originPanel == null || helpPanel == null) return;
+
+        foreach (Button btn in originPanel.GetComponentsInChildren<Button>(true))
+        {
+            if (btn.CompareTag("helpButton"))
+            {
+                btn.onClick.AddListener(SwitchToHelp);
+                break;
+            }
+        }
+    }
+
+    void SwitchToHelp()
+    {
+        originPanel.SetActive(false);
+        helpPanel.SetActive(true);
+    }
+}
+
